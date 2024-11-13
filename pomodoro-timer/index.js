fbb67d7b63c68e1c0cce0ebea3e0b1565dbd1005
Sarhad Bahrami <Sarhad94127@hotmail.com>
2024-11-13 09:58:10 +0100

    added timer project

---
+++ pomodoro-timer/index.js
@@ -0,0 +1,37 @@
+const timerElement = document.getElementById('timer');
+const startBtnElement = document.getElementById('start');
+const stopBtnElement = document.getElementById('stop');
+const resetBtnElement = document.getElementById('reset');
+
+let interval;
+let timeLeft = 1500;
+
+function updateTimer() {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  let formattedTime = `${minutes.toString().padStart(2, '0')}: ${seconds.toString().padStart(2, '0')}`;
+  timerElement.innerHTML = formattedTime;
+}
+
+function startTimer() {
+  interval = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      alert("Time's up!");
+      timeLeft = 1500;
+      updateTimer();
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  timeLeft = 1500;
+  updateTimer();
+}
